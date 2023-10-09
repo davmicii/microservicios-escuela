fb840f8a39fe07fcbd00c35001cecd4fd132b64a
danielvelezUTEQ <velezmirandadaniel@gmail.com>
2023-10-08 22:37:09 -0500

    auth-jwt

--- gateway-service/src/main/java/com/escuela/gatewayservice/Config/AuthFilter.java
+++ gateway-service/src/main/java/com/escuela/gatewayservice/Config/AuthFilter.java
@@ -1,20 +1,42 @@
+package com.escuela.gatewayservice.Config;
+
+import com.escuela.gatewayservice.Dto.TokenDto;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 @Component
-public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilterFactory.Config> {
-
-    public AuthFilter() {
-        super(Config.class);
+public class AuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String token = getTokenFromRequest(request);
+        if(token==null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        filterChain.doFilter(request,response);
     }
 
-    @Override
-    public GatewayFilter apply(AuthFilterFactory.Config config) {
+    private String getTokenFromRequest(HttpServletRequest request){
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")){
+            return authHeader.substring(7);
+        }
         return null;
     }
 
-    public static class Config {
-        // Puedes agregar configuración adicional para tu filtro aquí si es necesario
-    }
 }
